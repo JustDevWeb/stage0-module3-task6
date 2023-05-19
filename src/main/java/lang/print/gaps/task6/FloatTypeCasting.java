85b131b04872a9ff10c72b108bbec64897c7cd7e
JustDevWeb <zm91672@gmail.com>
2023-05-20 00:47:17 +0800

    dev: task 6  complete

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -2,5 +2,8 @@ package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
+        int roundFloat = Math.round(numberToBeRounded);
+
+        System.out.println(roundFloat);
     }
 }
